037dfcdd61d54a35923bd98c39571af8dceb2411
faiqilmupadi <159674288+faiqilmupadi@users.noreply.github.com>
2024-03-04 11:38:40 +0700

    Add files via upload

---
+++ Pratikum 3/AngkaSial.java
@@ -0,0 +1,28 @@
+/* File : Asersi1.java */
+/* Deskripsi : Program penggunaan exception buatan sendiri 
+               Pengenalan klausa 'throw' dan 'throws' */
+/* NIM / Nama : 24060122140144 / Muhammad faiq assajjad*/
+
+public class AngkaSial{
+    
+    public void cobaAngka(int angka) throws AngkaSialException{
+        if(angka == 13){
+            throw new AngkaSialException();
+        }
+        System.out.println(angka + "bukan angka sial");
+    }
+
+    public static void main(String[] args){
+        AngkaSial as = new AngkaSial();
+        try{
+            as.cobaAngka(10);
+            as.cobaAngka(13);
+            as.cobaAngka(12);
+        }
+        catch (AngkaSialException ase){
+            //method getMessage() telah ada pada kelas "Exception"
+            System.out.println(ase.getMessage());
+            System.out.println("hati - hati memasukkan angka!!");
+        }
+    }
+}
---
+++ Pratikum 3/AngkaSialException.java
@@ -0,0 +1,10 @@
+/* File : Asersi1.java */
+/* Deskripsi : Exception usaha sendiri nih, menolak angka 13 */
+/* NIM / Nama : 24060122140144 / Muhammad faiq assajjad*/
+
+public class AngkaSialException extends Exception{
+    public AngkaSialException(){
+        super("jangan memasukkan angka 13 karena sial broo!!");
+
+    }
+}
