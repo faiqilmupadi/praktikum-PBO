4820e7b2253fb66c022195bdde7573b7c80960ce
faiqilmupadi <159674288+faiqilmupadi@users.noreply.github.com>
2024-03-18 23:06:30 +0700

    Add files via upload

---
+++ Praktikum 4/Poligon.java
@@ -0,0 +1,13 @@
+// Nama      : MUhammad Faiq Assajjad
+// NIM       : 24060122140116
+// Tanggal   : 17 Maret 2024
+
+package org.poligon;
+
+public class Poligon {
+    protected int jumlahSisi;
+
+    public int getJumlahSisi(){
+        return this.jumlahSisi;
+    }
+}
